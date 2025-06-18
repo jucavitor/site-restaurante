6e04f212d7cbaff3da4faa53fd38c557c3dec330
jucavitor <jo4ovitor2005@gmail.com>
2025-06-18 11:09:15 -0300

    Inicializamos a estrutura basica do projeto

---
+++ cadastrar.php
@@ -0,0 +1,65 @@
+<?php
+if(isset($_POST['submit'])){
+
+//print_r($_POST['nome']);
+//print_r($_POST['telefone']);
+//print_r($_POST['endereco']);
+//print_r($_POST['email']);
+//print_r($_POST['senha']);
+
+include_once('config.php');
+
+$nome = $_POST['nome'];
+$telefone = $_POST['telefone'];
+$endereco = $_POST['endereco'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+
+$resultado = mysqli_query($conexao, "INSERT INTO clientes(nome, telefone, endereco, email, senha) VALUES ('$nome', '$telefone', '$endereco', '$email', '$senha')");
+
+}
+?>
+<!DOCTYPE html>
+<html lang="PT-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cadastrar</title>
+        <link rel="stylesheet" href="login.css">
+    </head>
+<body>
+    <nav>
+      <a href="login.php">Login</a>
+      <a href="home.php">home</a>
+      <a href="">Configurações</a>
+      <a href="carrinho.php">Meu pedidos</a>
+    </nav>
+
+    <form action="cadastrar.php" method="POST">
+        <fieldset>
+            <legend>Cadastrar clientes</legend>
+                <table>
+                    <tr>
+                        <td>Insira seu nome: <input type="text" name="nome"></td>
+                    </tr>
+                    <tr>
+                        <td>Insira seu Telefone: <input type="text" name="telefone"></td>
+                    </tr>
+                    <tr>
+                        <td>Insira seu endereço: <input type="text" name="endereco"></td>
+                    </tr>
+                    <tr>
+                        <td>Insira seu email: <input type="text" name="email"></td>
+                    </tr>
+                    <tr>
+                        <td>Crie uma senha: <input type="password" name="senha"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" name="submit"></td>
+                    </tr>
+                </table>
+        </fieldset>
+    </form>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Home</title>
+      <link rel="stylesheet" href="home.css">
+      <link rel="shortcut icon" href="imagens/icone.jpg" type="image/x-icon">
+   </head>
+<body>
+   <nav>
+      <a href="login.php">Login</a>
+      <a href="cadastrar.php">Cadastrar</a>
+      <a href="">Configurações</a>
+      <a href="carrinho.php">Meu pedidos</a>
+   
+   </nav>
+   <img src="imagens/restaurente.jpg" alt="">
+
+   <footer>
+      <p>&copy; 2025 Restaurante Delicioso - Todos os direitos reservados</p>
+   </footer>
+</body>
+</html>
