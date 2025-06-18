ca4b24193e410b5c6fde4ab4f8277ce15e326456
jucavitor <jo4ovitor2005@gmail.com>
2025-06-18 11:39:50 -0300

    adicionado arquivo de funções no php, e adicionado função simples que cadastra o usuario com procedure

---
+++ funcoes.php
@@ -0,0 +1,13 @@
+<?php
+// NESSE ARQUIVO IREI TRABALHAR COM AS PRINCIPAIS FUNÇÕES RELACIONADAS A BANCO DE DADOS.
+
+// 01 CADASTRAR USUARIO AO BANCO DE DADOS
+
+function($nome,$telefone,$senha,$endereço,$email,){
+
+$query = "";
+}
+
+
+
+?>
